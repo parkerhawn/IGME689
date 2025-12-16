afd4deef2226139e9814434ee1dfcea3c4db6234
parkerhawn <145530000+parkerhawn@users.noreply.github.com>
2025-12-16 05:27:02 -0500

    final changes

---
+++ Homework3/Assets/Scripts/AStar/PlayerNavMesh.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerNavMesh : MonoBehaviour
+{
+    [SerializeField] private Transform movePositionTransform;
+    private NavMeshAgent agent;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        agent.destination = movePositionTransform.position;
+    }
+}
